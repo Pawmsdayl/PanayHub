d0534aa081cc2864022892397bce585aaea77c75
Jeremy Jobert Peñafiel <jeremyjobert.penafiel@gmail.com>
2025-04-23 21:39:17 +0800

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Navbar from "./components/Navbar.tsx";
+import LandingPage from "./LandingPage.tsx";
+import Footer from "./components/Footer.tsx";
 
 function App() {
 
   return (
     <>
-      <div className={`flex w-full h-full outline`}>
+      <div className={`flex w-full min-h-screen  flex flex-col bg-white`}>
         <Navbar></Navbar>
+        <LandingPage></LandingPage>
+          <Footer></Footer>
       </div>
     </>
   )
