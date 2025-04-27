f2d1b387374f5e3eee3dc1bcf8aaf7ad7aa367c0
Jeremy Jobert Peñafiel <jeremyjobert.penafiel@gmail.com>
2025-04-28 00:03:34 +0800

    Update Navbar.tsx

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,9 +6,10 @@ function Navbar(){
       <nav className={`h-32 w-full navbar sticky z-50`}>
         <header className={`bg-white w-full h-full flex items-center`}>
           <div className={`w-full flex flex-row justify-between mx-10 items-center`}>
-            <h1 className={`text-3xl text-black font-bold`}>
-              PanayHub
-            </h1>
+            <div className={`text-black font-bold text-4xl flex gap-3 place-content-center`}>
+              <img src="src/assets/panayhub-logo.png" alt={`panayhub logo`}/>
+              <h1>PanayHub</h1>
+            </div>
             <div className={`text-(--body-font-color) flex gap-9 items-center font-bold`}>
               <p>Home</p>
               <p>Contact Us</p>
