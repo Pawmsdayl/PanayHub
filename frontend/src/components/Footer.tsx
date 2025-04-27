766bf9f3e5a6d259def96be69edb313ae7ac1db2
Jeremy Jobert Peñafiel <jeremyjobert.penafiel@gmail.com>
2025-04-28 00:03:38 +0800

    Update Footer.tsx

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,8 +2,35 @@
 function Footer(){
 
   return(
-    <div className={`w-full bg-(--body-font-color)`}>
-      hello
+    <div className={`w-full bg-(--body-font-color) h-72`}>
+      <div className={`grid grid-cols-3 place-items-center outline-2 outline-yellow-500 h-full`}>
+        <div className={`text-(--body-font-highlight) font-serif text-7xl`}>Contact Us.</div>
+
+        <div className={`flex flex-col`}>
+          <h1 className={`font-serif font-bold text-white text-2xl`}>Center for<br/>
+            West Visayan <br/>
+            Studies </h1>
+          <div className={`flex`}>
+            <p>pdcordero@up.edu.ph</p>
+          </div>
+          <div className={`flex`}>
+            <p>+63 9214605606</p>
+          </div>
+        </div>
+        <div>
+          <h1 className={`font-serif font-bold text-white text-2xl`}>
+            University of the <br/>
+            Philippines Visayas - <br/>
+            Division of Humanities
+          </h1>
+         <div className={`flex`}>
+            <p>pdcordero@up.edu.ph</p>
+          </div>
+          <div className={`flex`}>
+            <p>+63 9214605606</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
