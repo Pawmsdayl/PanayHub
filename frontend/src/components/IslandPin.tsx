45d5c2f41e2ce2891c20e84c3e7f83c0f6928534
Jeremy Jobert Peñafiel <jeremyjobert.penafiel@gmail.com>
2025-05-04 12:57:04 +0800

    remove hard code

--- frontend/src/components/IslandPin.tsx
+++ frontend/src/components/IslandPin.tsx
@@ -1,11 +1,11 @@
-function IslandPin ({className}: {className:string}) {
+function IslandPin ({className, imagePath}: {className:string, imagePath: string}) {
 
   return (
     <div className={className}>
       <div className={`flex place-content-center  w-fit relative h-20`}>
         <img className={``} src="src/assets/pin%201.png" alt={`pin`}/>
         <div className={`absolute w-fit top-0 left-10`}>
-          <img className={`object-scale-down`} src={"src/assets/panayhub-logo.png"} alt={`inner image`}/>
+          <img className={`object-scale-down`} src={imagePath} alt={`inner image`}/>
         </div>
       </div>
     </div>
