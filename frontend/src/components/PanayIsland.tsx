848c0b93595b740f5c8f95abc6071676e49c2db3
Jeremy Jobert Peñafiel <jeremyjobert.penafiel@gmail.com>
2025-05-05 15:45:57 +0800

    change asset names

--- frontend/src/components/PanayIsland.tsx
+++ frontend/src/components/PanayIsland.tsx
@@ -4,13 +4,13 @@ function PanayIsland () {
   return (
     <div className={`grid grid-rows-6 grid-cols-6 place-items-center`}>
       <img className={`col-span-full row-span-full object-scale-down`} src="src/assets/panay-island.png" alt={`panay island`}/>
-      <IslandPin imagePath="src/assets/monster.png" className={`row-start-1 col-start-1 col-span-2`}></IslandPin>
-      <IslandPin imagePath="src/assets/antique.png" className={`row-start-3 col-start-2 col-span-2 mr-10`}></IslandPin>
-      <IslandPin imagePath="src/assets/upv.png" className={`row-start-4 col-start-3 row-span-2 self-end mb-10`}></IslandPin>
-      <IslandPin imagePath="src/assets/cwvs.png" className={`row-start-4 col-start-4 row-span-2 mb-5`}></IslandPin>
-      <IslandPin imagePath="src/assets/monster.png" className={`row-start-2 col-start-5 row-span-2 mb-10`}></IslandPin>
+      <IslandPin imagePath="src/assets/monster.png" className={`row-start-1 col-start-2 row-span-2 mb-15 col`}></IslandPin>
+      <IslandPin imagePath="src/assets/bukig.png" className={`row-start-3 col-start-2 col-span-2`}></IslandPin>
+      <IslandPin imagePath="src/assets/upv.png" className={`row-start-4 col-start-3 row-span-2 col-span-2 mr-8`}></IslandPin>
+      <IslandPin imagePath="src/assets/cwvs.png" className={`row-start-4 col-start-3 row-span-2 mb-9 ml-22 col-span-2`}></IslandPin>
+      <IslandPin imagePath="src/assets/monster.png" className={`row-start-2 col-start-4 row-span-2 col-span-2 ml-20`}></IslandPin>
       <div className={`flex items-center justify-center bg-white size-40 rounded-full col-start-3 row-start-2 row-span-3 col-span-2 m`}>
-        <img className={`size-20`} src={"src/assets/panayhub-logo.png"} alt={`inner image`}/>
+        <img alt={`panayhub circle`} src={`src/assets/panayhub-circle.png`}/>
       </div>
     </div>
   );
