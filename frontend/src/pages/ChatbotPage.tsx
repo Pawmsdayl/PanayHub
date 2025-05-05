c05e2d779cc8d5d923255380dd3a9cef8d54f838
Jeremy Jobert Peñafiel <jeremyjobert.penafiel@gmail.com>
2025-05-05 15:46:20 +0800

    remove border

--- frontend/src/pages/ChatbotPage.tsx
+++ frontend/src/pages/ChatbotPage.tsx
@@ -4,7 +4,7 @@ function ChatbotPage() {
   return (
     <div className={`w-full h-[calc(100vh-128px)] bg-(--chatbot-bg-color)`}>
      <div className={`flex w-full h-full`}>
-       <div className={`flex w-full h-full gap-3 border-2 border-red-200 place-content-center p-10`}>
+       <div className={`flex w-full h-full gap-3  place-content-center p-10`}>
          <div className={`text-white bg-chatbot-light h-full w-full rounded-full text-center content-center`}>hello</div>
          <Chatbox></Chatbox>
          <div className={`text-white bg-chatbot-light h-full w-full text-center content-center`}>hello</div>
