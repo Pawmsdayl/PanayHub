1841d2bf72782541e6404bfd90e215de680f9e5b
Jeremy Jobert Peñafiel <jeremyjobert.penafiel@gmail.com>
2025-05-08 02:54:52 +0800

    try resizeing text area

--- frontend/src/components/InputBar.tsx
+++ frontend/src/components/InputBar.tsx
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import {AutosizeTextarea} from "@/components/AutosizeTextArea.tsx";
+import AutoResizingTextarea, {AutosizeTextarea} from "@/components/AutosizeTextArea.tsx";
 
 
 function InputBar({handleSendMessage}: {handleSendMessage: (message: string) => void}){
@@ -20,11 +20,12 @@ function InputBar({handleSendMessage}: {handleSendMessage: (message: string) => 
 
   return(
     <div className={`flex relative self-end h-15 w-full bg-chatbot-bg shadow-input-bar rounded-full`}>
-      <textarea className={`w-full h-20 resize-none text-white p-5 placeholder-[#96C7FD] outline-none`}
+      <AutoResizingTextarea className={`w-full h-20 resize-none text-white p-5 placeholder-[#96C7FD] outline-none`}
              value={value}
              placeholder={`Ask anything...`}
-             onInput={handleChange}>
-      </textarea>
+             // onInput={handleChange}
+      >
+      </AutoResizingTextarea>
       {/*<AutosizeTextarea/>*/}
       <button className={`top-1 right-1 size-12 absolute text-black bg-white rounded-full cursor-pointer font-bold`}
               type={`button`}
